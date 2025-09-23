92373df944d41e463a65e1a2c930515cc44e4de1
Julinium <julinium@gmail.com>
2025-09-24 00:47:08 +0100

    models update

--- scraper/models.py
+++ scraper/models.py
@@ -1,15 +1,9 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
+import uuid
 from django.db import models
 
 
 class Agrement(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     short = models.CharField(max_length=32, blank=True, null=True)
     name = models.CharField(max_length=512, blank=True, null=True)
     description = models.CharField(max_length=128, blank=True, null=True)
@@ -19,7 +13,7 @@ class Agrement(models.Model):
 
 
 class Category(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     label = models.CharField(max_length=32, blank=True, null=True)
     rank = models.SmallIntegerField(blank=True, null=True)
     description = models.CharField(max_length=128, blank=True, null=True)
@@ -29,7 +23,8 @@ class Category(models.Model):
 
 
 class Change(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    tender = models.ForeignKey('Tender', on_delete=models.DO_NOTHING, related_name="changes", db_column='tender', blank=True, null=True)    
     reported = models.DateTimeField(blank=True, null=True)
     field = models.CharField(max_length=64, blank=True, null=True)
     old_val = models.CharField(max_length=256, blank=True, null=True)
@@ -41,7 +36,7 @@ class Change(models.Model):
 
 
 class Client(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     short = models.CharField(max_length=32, blank=True, null=True)
     name = models.CharField(max_length=512, blank=True, null=True)
     ministery = models.CharField(max_length=16, blank=True, null=True)
@@ -52,7 +47,7 @@ class Client(models.Model):
 
 
 class Contact(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     when = models.DateTimeField(blank=True, null=True)
     ua = models.CharField(max_length=256, blank=True, null=True)
     ip = models.CharField(max_length=48, blank=True, null=True)
@@ -67,14 +62,14 @@ class Contact(models.Model):
     actions = models.CharField(max_length=512, blank=True, null=True)
     solved = models.BooleanField(blank=True, null=True)
     file = models.CharField(max_length=512, blank=True, null=True)
-    id_utilizer = models.ForeignKey('Utilizer', models.DO_NOTHING, db_column='id_utilizer', blank=True, null=True)
+    utilizer = models.ForeignKey('Utilizer', on_delete=models.DO_NOTHING, related_name="contacts", db_column='utilizer', blank=True, null=True)
 
     class Meta:
         db_table = 'contact'
 
 
 class Domain(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     short = models.CharField(max_length=32, blank=True, null=True)
     name = models.CharField(max_length=512, blank=True, null=True)
     description = models.CharField(max_length=128, blank=True, null=True)
@@ -84,60 +79,62 @@ class Domain(models.Model):
 
 
 class Download(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     when = models.DateTimeField(blank=True, null=True)
     bytes_media = models.BigIntegerField(blank=True, null=True)
     ua = models.CharField(max_length=256, blank=True, null=True)
     ip = models.CharField(max_length=48, blank=True, null=True)
-    id_tender = models.ForeignKey('Tender', models.DO_NOTHING, db_column='id_tender', blank=True, null=True)
-    id_utilizer = models.ForeignKey('Utilizer', models.DO_NOTHING, db_column='id_utilizer', blank=True, null=True)
+    tender = models.ForeignKey('Tender', on_delete=models.DO_NOTHING, related_name="downloads", db_column='tender', blank=True, null=True)
+    utilizer = models.ForeignKey('Utilizer', on_delete=models.DO_NOTHING, related_name="downloads", db_column='utilizer', blank=True, null=True)
 
     class Meta:
         db_table = 'download'
 
 
 class Favo(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     when = models.DateTimeField(blank=True, null=True)
     comment = models.CharField(max_length=512, blank=True, null=True)
     ua = models.CharField(max_length=256, blank=True, null=True)
     ip = models.CharField(max_length=48, blank=True, null=True)
-    id_utilizer = models.ForeignKey('Utilizer', models.DO_NOTHING, db_column='id_utilizer', blank=True, null=True)
-    id_tender = models.ForeignKey('Tender', models.DO_NOTHING, db_column='id_tender', blank=True, null=True)
+    utilizer = models.ForeignKey('Utilizer', on_delete=models.DO_NOTHING, related_name="favos", db_column='utilizer', blank=True, null=True)
+    tender = models.ForeignKey('Tender', on_delete=models.DO_NOTHING, related_name="favos", db_column='tender', blank=True, null=True)
 
     class Meta:
         db_table = 'favo'
 
 
 class Lot(models.Model):
-    id = models.UUIDField(primary_key=True)
-    number = models.SmallIntegerField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    number = models.SmallIntegerField(blank=True, null=True)
     title = models.CharField(max_length=512)
     description = models.CharField(max_length=512, blank=True, null=True)
-    estimate = models.BigIntegerField(blank=True, null=True, db_comment='In cents')
-    bond = models.BigIntegerField(blank=True, null=True)
+    estimate = models.DecimalField(max_digits=16, decimal_places=2, blank=True, null=True)
+    bond = models.DecimalField(max_digits=16, decimal_places=2, blank=True, null=True)
     reserved = models.BooleanField(blank=True, null=True)
     variant = models.BooleanField(blank=True, null=True)
-    plans_price = models.BigIntegerField(blank=True, null=True)
-    id_category = models.ForeignKey(Category, models.DO_NOTHING, db_column='id_category', blank=True, null=True)
-    id_tender = models.ForeignKey('Tender', models.DO_NOTHING, db_column='id_tender', blank=True, null=True)
+    plans_price = models.DecimalField(max_digits=16, decimal_places=2, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, related_name="lots", db_column='category', blank=True, null=True)
+    tender = models.ForeignKey('Tender', on_delete=models.DO_NOTHING, related_name="lots", db_column='tender', blank=True, null=True)
+    agrements = models.ManyToManyField('Agrement', through='RelAgrementLot', related_name='lots')
+    qualifs = models.ManyToManyField('Qualif', through='RelQualifLot', related_name='lots')
 
     class Meta:
         db_table = 'lot'
 
 
 class Meeting(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     time = models.DateTimeField(blank=True, null=True)
     description = models.CharField(max_length=512, blank=True, null=True)
-    id_lot = models.ForeignKey(Lot, models.DO_NOTHING, db_column='id_lot', blank=True, null=True)
+    lot = models.ForeignKey(Lot, on_delete=models.DO_NOTHING, related_name="meetings", db_column='lot', blank=True, null=True)
 
     class Meta:
         db_table = 'meeting'
 
 
 class Mode(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     short = models.CharField(max_length=32, blank=True, null=True)
     name = models.CharField(max_length=512, blank=True, null=True)
     description = models.CharField(max_length=128, blank=True, null=True)
@@ -147,7 +144,7 @@ class Mode(models.Model):
 
 
 class Procedure(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     short = models.CharField(max_length=32, blank=True, null=True)
     name = models.CharField(max_length=512, blank=True, null=True)
     description = models.CharField(max_length=128, blank=True, null=True)
@@ -157,7 +154,7 @@ class Procedure(models.Model):
 
 
 class Qualif(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     short = models.CharField(max_length=32, blank=True, null=True)
     name = models.CharField(max_length=512, blank=True, null=True)
     domain = models.CharField(max_length=32, blank=True, null=True)
@@ -169,53 +166,47 @@ class Qualif(models.Model):
 
 
 class RelAgrementLot(models.Model):
-    pk = models.CompositePrimaryKey('id_agrement', 'id_lot')
-    id_agrement = models.ForeignKey(Agrement, models.DO_NOTHING, db_column='id_agrement')
-    id_lot = models.ForeignKey(Lot, models.DO_NOTHING, db_column='id_lot')
+    pk = models.CompositePrimaryKey('agrement', 'lot')
+    agrement = models.ForeignKey(Agrement, on_delete=models.DO_NOTHING, db_column='agrement')
+    lot = models.ForeignKey(Lot, on_delete=models.DO_NOTHING, db_column='lot')
 
     class Meta:
         db_table = 'rel_agrement_lot'
-
-
-class RelChangeTender(models.Model):
-    pk = models.CompositePrimaryKey('id_change', 'id_tender')
-    id_change = models.ForeignKey(Change, models.DO_NOTHING, db_column='id_change')
-    id_tender = models.ForeignKey('Tender', models.DO_NOTHING, db_column='id_tender')
-
-    class Meta:
-        db_table = 'rel_change_tender'
+        unique_together = ('agrement', 'lot')
 
 
 class RelDomainTender(models.Model):
-    pk = models.CompositePrimaryKey('id_domain', 'id_tender')
-    id_domain = models.ForeignKey(Domain, models.DO_NOTHING, db_column='id_domain')
-    id_tender = models.ForeignKey('Tender', models.DO_NOTHING, db_column='id_tender')
+    pk = models.CompositePrimaryKey('domain', 'tender')
+    domain = models.ForeignKey(Domain, on_delete=models.DO_NOTHING, db_column='domain')
+    tender = models.ForeignKey('Tender', on_delete=models.DO_NOTHING, db_column='tender')
 
     class Meta:
         db_table = 'rel_domain_tender'
+        unique_together = ('domain', 'tender')
 
 
-class RelQualifOt(models.Model):
-    pk = models.CompositePrimaryKey('id_qualif', 'id_lot')
-    id_qualif = models.ForeignKey(Qualif, models.DO_NOTHING, db_column='id_qualif')
-    id_lot = models.ForeignKey(Lot, models.DO_NOTHING, db_column='id_lot')
+class RelQualifLot(models.Model):
+    pk = models.CompositePrimaryKey('qualif', 'lot')
+    qualif = models.ForeignKey(Qualif, on_delete=models.DO_NOTHING, db_column='qualif')
+    lot = models.ForeignKey(Lot, on_delete=models.DO_NOTHING, db_column='lot')
 
     class Meta:
-        db_table = 'rel_qualif_ot'
+        db_table = 'rel_qualif_lot'
+        unique_together = ('qualif', 'lot')
 
 
 class Sample(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     time = models.DateTimeField(blank=True, null=True)
     description = models.CharField(max_length=512, blank=True, null=True)
-    id_lot = models.ForeignKey(Lot, models.DO_NOTHING, db_column='id_lot', blank=True, null=True)
+    lot = models.ForeignKey(Lot, on_delete=models.DO_NOTHING, db_column='lot', blank=True, null=True)
 
     class Meta:
         db_table = 'sample'
 
 
 class Tender(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     chrono = models.CharField(max_length=16, blank=True, null=True)
     title = models.CharField(max_length=1024, blank=True, null=True)
     reference = models.CharField(max_length=128, blank=True, null=True)
@@ -237,17 +228,17 @@ class Tender(models.Model):
     updated = models.DateTimeField(blank=True, null=True)
     cancelled = models.BooleanField(blank=True, null=True)
     link = models.CharField(max_length=512, blank=True, null=True)
-    id_mode = models.ForeignKey(Mode, models.DO_NOTHING, db_column='id_mode', blank=True, null=True)
-    id_procedure = models.ForeignKey(Procedure, models.DO_NOTHING, db_column='id_procedure', blank=True, null=True)
-    id_client = models.ForeignKey(Client, models.DO_NOTHING, db_column='id_client', blank=True, null=True)
-    id_type = models.ForeignKey('Type', models.DO_NOTHING, db_column='id_type', blank=True, null=True)
+    mode = models.ForeignKey(Mode, on_delete=models.DO_NOTHING, db_column='mode', blank=True, null=True)
+    procedure = models.ForeignKey(Procedure, on_delete=models.DO_NOTHING, db_column='procedure', blank=True, null=True)
+    client = models.ForeignKey(Client, on_delete=models.DO_NOTHING, db_column='client', blank=True, null=True)
+    type = models.ForeignKey('Type', on_delete=models.DO_NOTHING, db_column='type', blank=True, null=True)
 
     class Meta:
         db_table = 'tender'
 
 
 class Type(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     short = models.CharField(max_length=32, blank=True, null=True)
     name = models.CharField(max_length=512, blank=True, null=True)
     description = models.CharField(max_length=128, blank=True, null=True)
@@ -257,7 +248,7 @@ class Type(models.Model):
 
 
 class Utilizer(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=64)
     email = models.CharField(blank=True, null=True)
 
@@ -266,10 +257,10 @@ class Utilizer(models.Model):
 
 
 class Visit(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     time = models.DateTimeField(blank=True, null=True)
     description = models.CharField(max_length=512, blank=True, null=True)
-    id_lot = models.ForeignKey(Lot, models.DO_NOTHING, db_column='id_lot', blank=True, null=True)
+    lot = models.ForeignKey(Lot, on_delete=models.DO_NOTHING, db_column='lot', blank=True, null=True)
 
     class Meta:
         db_table = 'visit'
