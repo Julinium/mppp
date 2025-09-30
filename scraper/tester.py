b54399f79c4655999ec342935485dbcccf65a9ba
Julinium <julinium@gmail.com>
2025-09-30 13:38:59 +0100

    tests

--- scraper/tester.py
+++ scraper/tester.py
@@ -1,24 +1,18 @@
-import os, sys
-import django
+# import os, sys
+# import django
 
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(project_root)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mppp.settings')
-django.setup()
+# project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# sys.path.append(project_root)
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mppp.settings')
+# django.setup()
 
 
-from scraper.models import Category, Tender
+# from scraper.models import Category, Tender
 
-uts = Tender.objects.filter(category__label=None)
-i = 0
-n = len(uts)
-print(f"Found items: {n:05}\n")
+# uts = Tender.objects.filter(category__label=None)
+# i = 0
+# n = len(uts)
+# print(f"Found items: {n:05}\n")
 
-deleted_count, _ = uts.delete()
-print(f"Deleted {deleted_count} objects")
-
-# for t in uts:
-#     i += 1
-#     print(f"Deleting {t.chrono} --------{i}/{n}-------")
-#     t.delete()
-#     print(f"--------------- Deleted {t.chrono}")
+# deleted_count, _ = uts.delete()
+# print(f"Deleted {deleted_count} objects")
